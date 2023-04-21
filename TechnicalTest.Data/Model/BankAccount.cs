0f75a93f1a2f4c7c58178f83fa4d52c2ba96878c
Richard Baker <richard.baker@bspokesoftware.io>
2023-04-21 15:29:33 +0100

    Fixed model relationships

--- TechnicalTest.Data/Model/BankAccount.cs
+++ TechnicalTest.Data/Model/BankAccount.cs
@@ -1,4 +1,6 @@
-﻿namespace TechnicalTest.Data.Model;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TechnicalTest.Data.Model;
 
 public class BankAccount : BaseUpdateAndDeleteModel<int>
 {
@@ -7,5 +9,9 @@ public class BankAccount : BaseUpdateAndDeleteModel<int>
     public decimal Balance { get; set; }
     
     public Customer? Customer { get; set; }
-    public ICollection<BankAccountFrozenStatus> Status { get; set; } = new HashSet<BankAccountFrozenStatus>();
+    public ICollection<BankAccountFrozenStatus> BankAccountFrozenStatuses { get; set; } = new HashSet<BankAccountFrozenStatus>();
+    [InverseProperty("FromBankAccount")]
+    public ICollection<BankAccountTransfer> FromBankAccountTransfers { get; set; } = new HashSet<BankAccountTransfer>();
+    [InverseProperty("ToBankAccount")]
+    public ICollection<BankAccountTransfer> ToBankAccountTransfers { get; set; } = new HashSet<BankAccountTransfer>();
 }
--- TechnicalTest.Data/Model/BankAccountTransfer.cs
+++ TechnicalTest.Data/Model/BankAccountTransfer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,11 @@ namespace TechnicalTest.Data.Model
 {
     public class BankAccountTransfer : BaseCreateModel<int>
     {
+      
         public int FromBankAccountId { get; set; }
+        public BankAccount FromBankAccount { get; set; }
         public int ToBankAccountId { get; set; }
+        public BankAccount ToBankAccount { get; set; }
         public decimal Amount { get; set; }
         public string? Reference { get; set; }
     }
