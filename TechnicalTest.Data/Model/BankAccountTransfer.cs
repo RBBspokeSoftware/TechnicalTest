dd1395b94e1928fa8058ec7c300c248c6e30d76e
Richard Baker <richard.baker@bspokesoftware.io>
2023-04-24 10:32:49 +0100

    Made class types nullable in BankAccountTransfer

--- TechnicalTest.Data/Model/BankAccountTransfer.cs
+++ TechnicalTest.Data/Model/BankAccountTransfer.cs
@@ -11,9 +11,9 @@ namespace TechnicalTest.Data.Model
     {
       
         public int FromBankAccountId { get; set; }
-        public BankAccount FromBankAccount { get; set; }
+        public BankAccount? FromBankAccount { get; set; }
         public int ToBankAccountId { get; set; }
-        public BankAccount ToBankAccount { get; set; }
+        public BankAccount? ToBankAccount { get; set; }
         public decimal Amount { get; set; }
         public string? Reference { get; set; }
     }
