9015d5044ba97159576b905df03933aca17b0e84
Richard Baker <richard.baker@bspokesoftware.io>
2023-04-24 11:24:21 +0100

    Added Nullable fields into the record objects where needed

--- TechnicalTest.API/Models/AddCustomerModel.cs
+++ TechnicalTest.API/Models/AddCustomerModel.cs
@@ -1,8 +1,8 @@
 namespace TechnicalTest.API.Models;
 
-public record AddCustomerModel(string FirstName, string MiddleNames, string LastName, DateTime DateOfBirth, decimal DailyTransferLimit);
+public record AddCustomerModel(string FirstName, string? MiddleNames, string LastName, DateTime DateOfBirth, decimal DailyTransferLimit);
 
-public record EditCustomerModel(int Id, string FirstName, string MiddleNames, string LastName, DateTime DateOfBirth, decimal DailyTransferLimit);
+public record EditCustomerModel(int Id, string FirstName, string? MiddleNames, string LastName, DateTime DateOfBirth, decimal DailyTransferLimit);
 
 public record AddBankAccountModel(int CustomerId, string AccountNumber, decimal Balance);
 
